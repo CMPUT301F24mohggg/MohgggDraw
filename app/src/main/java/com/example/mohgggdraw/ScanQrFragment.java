ec32d38c55a949fe11dddf565dfb6779a866f3b5
arktos <justinjc2005@gmail.com>
2024-11-07 11:43:50 -0700

    Created file

--- app/src/main/java/com/example/mohgggdraw/ScanQrFragment.java
+++ app/src/main/java/com/example/mohgggdraw/ScanQrFragment.java
@@ -1,4 +1,23 @@
 package com.example.mohgggdraw;
 
-public class ScanQrFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class ScanQrFragment extends Fragment {
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
 }
